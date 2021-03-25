1cc6e8e175e9825a6fa173f520c89b0341ced59c
Sayne Rojas <saynegrojas@yahoo.com>
2021-03-25 01:57:01 -0700

    created/swtiched to new branch

--- DatingApp.API/IdentityServiceExtension/IdentityServiceExtension.cs
+++ DatingApp.API/IdentityServiceExtension/IdentityServiceExtension.cs
@@ -9,8 +9,9 @@ namespace DatingApp.API.IdentityServiceExtension
     // will be adding all the services we added in startup regarding identities
     public static class IdentityServiceExtension
     {
-        public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration config) {
-            
+        public static IServiceCollection AddIdentityService(this IServiceCollection services, IConfiguration config)
+        {
+
             // add authentication services
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(options =>
